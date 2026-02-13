16d0cc2cbc9d8644439ce4208f9f451db8f864d5
ta5985-blip <ta5985@srmist.edu.in>
2026-02-13 21:24:37 +0530

    Rename Main class and add Javadoc comments
    Renamed Main class to OOPSBannerApp and added documentation.

--- Main.java
+++ Main.java
@@ -1,4 +1,18 @@
-public class Main {
+/**
+ * OOPSBannerApp
+ * 
+ * This application prints the literal text "OOPS" to the console.
+ * 
+ * @author Cheenu Mummurthy
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * 
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
